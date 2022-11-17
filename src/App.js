f28bd643ea66baa5019f5a38490325564d59b3a4
Jefferson França <jefferson.vieiraf@hotmail.com>
2022-11-16 23:19:53 -0300

    feat: store selected cards on list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import React, {useState} from "react";
+import CardGenerator from "./components/CardGenerator";
+
 function App() {
+  const [cardList, setCardList] = useState([]);
+  function update(id){
+    setCardList([...cardList, id]);
+  }
   return (
     <div className="App">
+      <p>Cards clicked: {cardList}</p>
+      <CardGenerator update={update}/>
     </div>
   );
 }
