b2f55ec8487d0a716e6803db0c9c94a43c41d5e2
Jefferson França <jefferson.vieiraf@hotmail.com>
2022-11-16 23:57:04 -0300

    feat: implement scoreboard

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import CardGenerator from "./components/CardGenerator";
 
 function App() {
   const [cardList, setCardList] = useState([]);
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
   function update(id){
     setCardList([...cardList, id]);
   }
+  const updateScoreboard = async () => {
+    setCurrentScore((currentScore) => {
+      if(currentScore + 1 > bestScore){
+        setBestScore(currentScore + 1);
+      }
+      return currentScore + 1;
+    })
+  }
+  useEffect(() => {
+    const newSet = new Set(cardList);
+    if(newSet.size !== cardList.length){
+      setCurrentScore(0);
+      setCardList([]);
+    }
+    else if(cardList.length){
+      updateScoreboard();
+    }
+  }, [cardList]);
   return (
     <div className="App">
-      <p>Cards clicked: {cardList}</p>
+      <p>Score: {currentScore} Best Score: {bestScore}</p>
       <CardGenerator update={update}/>
     </div>
   );
