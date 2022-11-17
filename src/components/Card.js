43aaf6f3c67af97c62c9add9a96149658b990960
Jefferson França <jefferson.vieiraf@hotmail.com>
2022-11-17 11:33:20 -0300

    refactor: implement background color on card

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,8 +2,8 @@ import React from "react"
 
 export default function Card({update, item}){
   return(
-    <div className="card" onClick={() => update(item.text)}>
-      <img src={item.src} />
+    <div className="card" style={{backgroundColor: item.color}} onClick={() => update(item.text)}>
+      <img src={item.src} alt={item.text}/>
       <span>{item.text}</span>
     </div>
   )
